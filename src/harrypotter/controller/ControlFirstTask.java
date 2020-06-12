14d26363bc4bb517993f9e0401a5d56cafc4f2da
NadeenAhmad <36262947+NadeenAhmad@users.noreply.github.com>
2020-06-12 14:27:58 +0200

    Add files via upload

---
+++ src/harrypotter/controller/ControlFirstTask.java
@@ -0,0 +1,29 @@
+package harrypotter.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import harrypotter.exceptions.InCooldownException;
+import harrypotter.exceptions.InvalidTargetCellException;
+import harrypotter.exceptions.OutOfBordersException;
+import harrypotter.view.FirstTaskView;
+
+public class ControlFirstTask {
+//	public ControlFirstTask(FirstTaskView x) {
+//		x.getUseTrait().addActionListener(new ActionListener() {
+//
+//			public void actionPerformed(ActionEvent ae) {
+//
+//				try {
+//					x.getTask1().getCurrentChamp().useTrait();
+//					// System.out.print("malak");
+//				} catch (InCooldownException | OutOfBordersException
+//						| InvalidTargetCellException | IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+}
---
+++ src/harrypotter/controller/HarryPotterGUI.java
@@ -0,0 +1,13 @@
+package harrypotter.controller;
+
+import java.io.IOException;
+
+import harrypotter.view.HarryPotterView;
+
+public class HarryPotterGUI {
+	public static void main(String[] args) throws IOException {
+		new HarryPotterView();
+
+	}
+
+}
